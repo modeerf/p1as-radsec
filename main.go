9206b08ac73afe39fbb3249aac321fe1e9899887
Jason Wilcox <jason@jasonsays.com>
2022-08-14 14:45:05 -0600

    ok maybe they weren't right

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	log "github.com/inconshreveable/log15"
+	logext "github.com/inconshreveable/log15/ext"
+	"github.com/kelseyhightower/envconfig"
+	server "github.com/modeerf/p1as-radsec"
+)
+
+var version string
+var showVersion bool
+
+func init() {
+	h := logext.FatalHandler(log.CallerFileHandler(log.StdoutHandler))
+	log.Root().SetHandler(h)
+	flag.BoolVar(&showVersion, "v", false, "Show version")
+	flag.Parse()
+}
+
+func main() {
+	if showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+	var conf server.ServerConfig
+	if err := envconfig.Process("grp", &conf); err != nil {
+		log.Crit("failed to load env", "err", err)
+	}
+	srv := server.New(&conf)
+	log.Crit("grp exited", "err", srv.ListenAndServe())
+}
--- server/server.go
+++ server/server.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	log "github.com/inconshreveable/log15"
-	radius "github.com/modeerf/p1as-radsec/radius"
+	radius "github.com/modeerf/p1as-radsec"
 )
 
 var testHookListenAndServe func(*Server, net.Listener) // used if non-nil
